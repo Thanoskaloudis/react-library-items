dfa269b954b125a45ceddab12d41500aa6e121e8
Thanoskaloudis <8anos.kal@gmail.com>
2022-10-09 20:19:31 +0100

    implement sorting

--- src/components/CardItems/CardItems.tsx
+++ src/components/CardItems/CardItems.tsx
@@ -1,25 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from '../Card/Card';
-import { ICardItems } from './CardItems.model';
+import { ICardItem, ICardItems } from './CardItems.model';
 import "./CardItems.scss";
 
 export const CardItems = (props: ICardItems) => {
   const [query, setQuery] = useState("");
+  const [sortState, setSortState] = useState("");
+  const [showingItems, setshowingItems] = useState<ICardItem[]>([]);
+
+  useEffect(() => {
+    setshowingItems(props.items);
+  }, [props])
 
   const updateQuery = (query: string) => {
     setQuery(query.trim());
-  };
 
-  const clearQuery = () => {
-    updateQuery("");
-  };
-
-  const showingItems =
-  query === ""
-    ? props.items
-    : props.items.filter((item) =>
+    if(query === "") {
+      setshowingItems(props.items);
+    } else {
+      setshowingItems(
+        props.items.filter((item) =>
         item.title.toLowerCase().includes(query.toLowerCase())
-      );
+      ))
+    }
+  };
+
+  const updateSortState = (event: string) => {
+    setSortState(event);
+
+    if(event === "duration") {
+      setshowingItems(sortItemsByDuration());
+    } else if(event === "alphabetical") {
+      setshowingItems(sortItemsByTitle());
+    }
+  };
+
+  const sortItemsByDuration = (): ICardItem[] => {
+    return showingItems.sort((a, b) => +a.duration.replace(/\D/g, "") - +b.duration.replace(/\D/g, ""));
+  }
+
+  const sortItemsByTitle = (): ICardItem[] => {
+    return showingItems.sort((a, b) => {
+      if (a.title > b.title) {
+          return 1;
+      }
+      if (b.title > a.title) {
+          return -1;
+      }
+      return 0;
+    });
+  }
 
   return (
     <div className="cards">
@@ -31,6 +61,14 @@ export const CardItems = (props: ICardItems) => {
             value={query}
             onChange={(event) => updateQuery(event.target.value)}
           />
+          <div className="cards--top__sort">
+            <label className="cards--top__sort__label">Sort By</label>
+            <select className="cards--top__sort__select" defaultValue={'DEFAULT'} onChange={(e) => updateSortState(e.target.value)}>
+              <option value="DEFAULT" disabled>None</option>
+              <option value="duration">Duration</option>
+              <option value="alphabetical">Alphabetical</option>
+          </select>
+          </div>
       </div>
       {showingItems.map((cardItem) => (
         <div className="cards--item" key={cardItem.id}>
