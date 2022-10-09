141b1c0f6db14825d59005d08bb53dba862b9b2b
Thanoskaloudis <8anos.kal@gmail.com>
2022-10-09 17:48:36 +0100

    add styles on card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { ICard } from './Card.model';
+import { RiBook2Fill } from 'react-icons/ri';
+import { FaArrowRight } from 'react-icons/fa';
 import "./Card.scss";
 
 export const Card = (props: ICard) => {
   return (
     <div className="card">
-      <div className="card__image-holder">
+      <div className="card--image"> 
         <img
-          className="card__image"
           src={props.imgURL}
           alt=""
         />
       </div>
-      <div className="card-title">
-        <span className="card-title__subtitle">{props.type}</span>
-        <span className="card-title__subtitle">{props.duration}</span>
-        <h2>{props.title}</h2>
+      <div className="card--title">
+        <span className="card--title__subtitle"> <RiBook2Fill /> {props.type} • {props.duration}</span>
+        <h2 className="card--title__title">{props.title}</h2>
       </div>
-      <div className="card-intro">
+      <div className="card--description">
       {props.description}
       </div>
-      <div>
-      <a href={props.link} target="_blank" rel="noopener noreferrer">View pathway</a>
+      <div className="card--link">
+        <a className="card--link__href" href={props.link} target="_blank" rel="noopener noreferrer">View pathway</a>
+        <FaArrowRight />
       </div>
     </div>
   );
