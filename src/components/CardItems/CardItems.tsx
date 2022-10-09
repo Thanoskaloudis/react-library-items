5db84cb06a74595839dab0542de9129582e6901f
Thanoskaloudis <8anos.kal@gmail.com>
2022-10-09 18:21:20 +0100

    add a search input for filtering

--- src/components/CardItems/CardItems.tsx
+++ src/components/CardItems/CardItems.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '../Card/Card';
 import { ICardItems } from './CardItems.model';
 import "./CardItems.scss";
 
 export const CardItems = (props: ICardItems) => {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query: string) => {
+    setQuery(query.trim());
+  };
+
+  const clearQuery = () => {
+    updateQuery("");
+  };
+
+  const showingItems =
+  query === ""
+    ? props.items
+    : props.items.filter((item) =>
+        item.title.toLowerCase().includes(query.toLowerCase())
+      );
+
   return (
     <div className="cards">
-      {props.items.map((cardItem) => (
-        <div className="cards-item" key={cardItem.id}>
+        <div className="cards--top">
+          <input
+            className="cards--top__search"
+            type="text"
+            placeholder="Search Library"
+            value={query}
+            onChange={(event) => updateQuery(event.target.value)}
+          />
+      </div>
+      {showingItems.map((cardItem) => (
+        <div className="cards--item" key={cardItem.id}>
           <Card
             title={cardItem.title}
             imgURL={cardItem.image}
